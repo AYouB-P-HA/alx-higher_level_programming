dec404a84e150ac787a6fe1dbbdfb0d63de75160
AYouB-P-HA <hawkridge00@gmail.com>
2024-03-12 14:46:28 -0700

    task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def uppercase(s):
+    result = ''
     for x in range(0, len(s)):
         char = s[x]
         i = ord(char)
         if i >= 97 and i < 123:
             j = i - 32
-            print('{}'.format(chr(j)), end="")
+            result += chr(j)
         else:
-            print('{}'.format(char), end="")
-    return print()
+            result += char
+    return print('{}'.format(result), end=" \n")
