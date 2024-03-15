4e3c279cbe3dd5867ad937b558c10a3ddb9311ba
AYouB-P-HA <hawkridge00@gmail.com>
2024-03-15 09:30:00 -0700

    opt-task

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,20 +6,19 @@ if __name__ == "__main__":
     i = len(args)
     if i != 3:
         print('usage: ./100-my_calculator.py <a> <operator> <b>')
-        sys.exit(1)
+        exit(1)
     else:
         a = int(args[0])
         b = int(args[2])
         operator = args[1]
-        match operator:
-            case '+':
+        if operator == '+':
                 print('{} + {} = {}'.format(a, b, add(a, b)))
-            case '-':
+        elif operator == '-':
                 print('{} - {} = {}'.format(a, b, sub(a, b)))
-            case '*':
+        elif operator == '*':
                 print('{} * {} = {}'.format(a, b, mul(a, b)))
-            case '/':
+        elif operator == '/':
                 print('{} / {} = {}'.format(a, b, div(a, b)))
-            case _:
-                print('Unknown operator. Available operators: +, -, * and /')
-                sys.exit(1)
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
