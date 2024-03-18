dadbed9f70b79d1011aa155659ca1d5580732aab
AYouB-P-HA <hawkridge00@gmail.com>
2024-03-18 14:52:19 -0700

    task

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+def new_in_list(my_list, idx, element): 
+    x = my_list.copy()
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+    else:
+        x[idx] = element
+        return x
