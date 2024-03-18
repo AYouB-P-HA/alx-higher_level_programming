ca409f8148a073aaf2e4ccd6957d84f96f78b7d6
AYouB-P-HA <hawkridge00@gmail.com>
2024-03-18 15:59:20 -0700

    task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    x = list (my_string.strip())
-    result = []
-    for char in x: 
+    str = ''
+    for char in my_string: 
         if char != 'c' and char != 'C':
-            result.append(char)
-    return ''.join(result)
+            str += char
+    return str
